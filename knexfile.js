99cc935071f9c0307e84367c82d0deb2315042f9
dfrancese <davefrancese@gmail.com>
2020-05-14 15:28:05 -0400

    try deploy

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// Update with your config settings.
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -9,35 +9,8 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
+    client: "pg",
+    connection: process.env.DATABASE_URL
   }
 };
